987c71859deff9344fed6e46146f52cbfb7d9e15
lab616 <dchung616@gmail.com>
2014-09-08 13:20:55 -0700

    SSH support for key file and ssh-agent; test cases; test keys for dev in GCE qoriolabsdev project

---
+++ environments/dev/.ssh/README.md
@@ -0,0 +1,1 @@
+The test public key has been added to the GCE project qoriolabsdev as one of its default SSH keys to include on instance creation.
--- util/ssh.go
+++ util/ssh.go
@@ -2,7 +2,11 @@ package util
 
 import (
 	"code.google.com/p/go.crypto/ssh"
+	"code.google.com/p/go.crypto/ssh/agent"
+	"errors"
 	"io/ioutil"
+	"net"
+	"os"
 )
 
 func ParseKey(file string) (ssh.Signer, error) {
@@ -17,8 +21,63 @@ func ParseKey(file string) (ssh.Signer, error) {
 	return private, nil
 }
 
-func ExecCommand(client *ssh.Client, cmd string) ([]byte, error) {
-	session, err := client.NewSession()
+type sshClient struct {
+	config *ssh.ClientConfig
+	client *ssh.Client
+}
+
+func SshKeyFileAuthMethod(keyfile string) (ssh.AuthMethod, error) {
+	key, err := ParseKey(keyfile)
+	if err != nil {
+		return nil, err
+	}
+	return ssh.PublicKeys(key), nil
+}
+
+func SshAgentAuthMethod() (ssh.AuthMethod, error) {
+	env := os.Getenv("SSH_AUTH_SOCK")
+	if env == "" {
+		return nil, errors.New("SSH_AUTH_SOCK not defined")
+	}
+	conn, err := net.Dial("unix", env)
+	if err != nil {
+		return nil, err
+	}
+	agent := agent.NewClient(conn)
+	auths, err := agent.Signers()
+	if err != nil {
+		return nil, err
+	}
+	return ssh.PublicKeys(auths...), nil
+}
+
+func SshKeyBytesAuthMethod(buff []byte) (ssh.AuthMethod, error) {
+	private, err := ssh.ParsePrivateKey(buff)
+	if err != nil {
+		return nil, err
+	}
+	return ssh.PublicKeys(private), nil
+}
+
+func NewSshClient(user, host string, auth ssh.AuthMethod) (*sshClient, error) {
+	c := &sshClient{
+		config: &ssh.ClientConfig{
+			User: user,
+			Auth: []ssh.AuthMethod{
+				auth,
+			},
+		},
+	}
+	client, err := ssh.Dial("tcp", host+":22", c.config)
+	if err != nil {
+		return nil, err
+	}
+	c.client = client
+	return c, nil
+}
+
+func (this *sshClient) RunCommandStdout(cmd string) ([]byte, error) {
+	session, err := this.client.NewSession()
 	if err != nil {
 		return nil, err
 	}
