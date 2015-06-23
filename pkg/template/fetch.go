da3adecdf8aae434ce6df1eee6c9cd29b7a3f1fe
David Chung <dchung@qoriolabs.com>
2015-06-23 10:06:42 -0700

    New template package

---
+++ pkg/template/fetch.go
@@ -0,0 +1,212 @@
+package template
+
+import (
+	"bytes"
+	"crypto/tls"
+	"errors"
+	"github.com/golang/glog"
+	"github.com/qorio/maestro/pkg/registry"
+	"github.com/qorio/maestro/pkg/zk"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+)
+
+var (
+	ErrNotSupportedProtocol = errors.New("protocol-not-supported")
+)
+
+func ParseHostPort(value string) (host, port string) {
+	parts := strings.Split(value, ":")
+	if len(parts) == 2 {
+		return parts[0], parts[1]
+	}
+	return "", ""
+}
+
+func FetchUrl(urlRef string, headers map[string]string, zc ...zk.ZK) (body string, mime string, err error) {
+	switch {
+	case strings.Index(urlRef, "http://") == 0, strings.Index(urlRef, "https://") == 0:
+		url, err := url.Parse(urlRef)
+		if err != nil {
+			return "", "", err
+		}
+
+		// don't check certificate for https
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		client := &http.Client{Transport: tr}
+		req, err := http.NewRequest("GET", url.String(), nil)
+
+		for h, v := range headers {
+			req.Header.Add(h, v)
+		}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			return "", "", err
+		}
+		content, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			return "", "", err
+		}
+		return string(content), resp.Header.Get("Content-Type"), nil
+
+	case strings.Index(urlRef, "file://") == 0:
+		file := urlRef[len("file://"):]
+		f, err := os.Open(file)
+		if err != nil {
+			return "", "", err
+		}
+		defer f.Close()
+		if buff, err := ioutil.ReadAll(f); err != nil {
+			return "", "", err
+		} else {
+			return string(buff), "text/plain", nil
+		}
+	case strings.Index(urlRef, "env://") == 0:
+		if len(zc) == 0 {
+			return "", "", errors.New("no-zk-client")
+		}
+		path := urlRef[len("env://"):]
+		n, err := zc[0].Get(path)
+		glog.Infoln("Content from environment: Path=", urlRef, "Err=", err)
+		if err != nil {
+			return "", "", err
+		} else {
+			return n.GetValueString(), "text/plain", nil
+		}
+	}
+	return "", "", ErrNotSupportedProtocol
+}
+
+func apply_template(key, tmpl string, data interface{}, funcMap ...template.FuncMap) ([]byte, error) {
+	t := template.New(key)
+	if len(funcMap) > 0 {
+		t = t.Funcs(funcMap[0])
+	}
+	t, err := t.Parse(tmpl)
+	if err != nil {
+		return nil, err
+	}
+	var buff bytes.Buffer
+	err = t.Execute(&buff, data)
+	return buff.Bytes(), err
+}
+
+func ExecuteTemplateUrl(zc zk.ZK, url string, authToken string, data interface{}) ([]byte, error) {
+	headers := map[string]string{
+		"Authorization": "Bearer " + authToken,
+	}
+
+	config_template_text, _, err := FetchUrl(url, headers, zc)
+	if err != nil {
+		glog.Warningln("Error fetching template:", err)
+		return nil, err
+	}
+
+	funcMap := template.FuncMap{
+		"containers": func(path, service_port string) ([]interface{}, error) {
+			// We support variables inside the function argument
+			p, err := apply_template(path, path, data)
+			if err != nil {
+				return nil, err
+			}
+			return hostport_list_from_zk(zc, string(p), service_port)
+		},
+		"inline": func(url string) (string, error) {
+			// We support variables inside the function argument
+			u, err := apply_template(url, url, data)
+			if err != nil {
+				return "", err
+			}
+			content, _, err := FetchUrl(string(u), headers, zc)
+			if err != nil {
+				return "", err
+			}
+			return content, nil
+		},
+		"file": func(url string, dir ...string) (string, error) {
+			// We support variables inside the function argument
+			u, err := apply_template(url, url, data)
+			if err != nil {
+				return "", err
+			}
+			content, _, err := FetchUrl(string(u), headers, zc)
+			if err != nil {
+				return "", err
+			}
+			// Write to local file and return the path
+			parent := os.TempDir()
+			if len(dir) > 0 {
+				parent = dir[0]
+				// We support variables inside the function argument
+				p, err := apply_template(parent, parent, data)
+				if err != nil {
+					return "", err
+				}
+				parent = string(p)
+			}
+			path := filepath.Join(parent, filepath.Base(string(u)))
+			err = ioutil.WriteFile(path, []byte(content), 0777)
+			glog.Infoln("Written", len([]byte(content)), " bytes to", path, "Err=", err)
+			if err != nil {
+				return "", err
+			}
+			return path, nil
+		},
+	}
+
+	config_template, err := template.New(url).Funcs(funcMap).Parse(config_template_text)
+	if err != nil {
+		glog.Warningln("Error parsing template", url, err)
+		return nil, err
+	}
+	var buff bytes.Buffer
+	err = config_template.Execute(&buff, data)
+	return buff.Bytes(), err
+}
+
+// Supports references -- if the value of the node is env:///.. then resolve the reference.
+func hostport_list_from_zk(zc zk.ZK, containers_path, service_port string) ([]interface{}, error) {
+	n, err := zc.Get(containers_path)
+	if err != nil {
+		return nil, err
+	}
+
+	// try resolve
+	p, _, err := zk.Resolve(zc, registry.Path(containers_path), n.GetValueString())
+	if p.Path() != containers_path {
+		n, err = zc.Get(p.Path())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	all, err := n.VisitChildrenRecursive(func(z *zk.Node) bool {
+		_, port := ParseHostPort(z.GetBasename())
+		return port == service_port && z.IsLeaf()
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	list := make([]interface{}, 0)
+	for _, c := range all {
+		host, port := ParseHostPort(c.GetValueString())
+		list = append(list, struct {
+			Host string
+			Port string
+		}{
+			Host: host,
+			Port: port,
+		})
+	}
+	return list, nil
+}
---
+++ pkg/template/fetch_test.go
@@ -0,0 +1,217 @@
+package template
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/qorio/maestro/pkg/registry"
+	"github.com/qorio/maestro/pkg/zk"
+	. "gopkg.in/check.v1"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func TestFetch(t *testing.T) { TestingT(t) }
+
+type TestSuiteFetch struct {
+	zc zk.ZK
+}
+
+var _ = Suite(&TestSuiteFetch{})
+
+// Database set up for circle_ci:
+// psql> create role ubuntu login password 'password';
+// psql> create database circle_ci with owner ubuntu encoding 'UTF8';
+func (suite *TestSuiteFetch) SetUpSuite(c *C) {
+	zc, err := zk.Connect([]string{"localhost:2181"}, 1*time.Second)
+	c.Assert(err, Equals, nil)
+	suite.zc = zc
+}
+
+func (suite *TestSuiteFetch) TearDownSuite(c *C) {
+	suite.zc.Close()
+}
+
+func (suite *TestSuiteFetch) TestFetchAndExecuteTemplate(c *C) {
+
+	list := make([]interface{}, 0)
+
+	for i := 0; i < 10; i++ {
+		port := 45167 + i
+		hostport := struct {
+			Host string
+			Port string
+		}{
+			Host: "ip-10-31-81-235",
+			Port: fmt.Sprintf("%d", port),
+		}
+		c.Log("instance= ", hostport)
+		list = append(list, hostport)
+	}
+
+	data := make(map[string]interface{})
+	data["HostPortList"] = list
+	c.Log("Data= ", data)
+
+	url := "http://qorio.github.io/public/nginx/nginx.conf"
+	config, err := ExecuteTemplateUrl(nil, url, "", data)
+
+	c.Assert(err, Equals, nil)
+	c.Log("config= ", string(config))
+}
+
+func (suite *TestSuiteFetch) TestFetchAndExecuteTemplate2(c *C) {
+
+	list := make([]interface{}, 0)
+
+	for i := 0; i < 10; i++ {
+		port := 45167 + i
+		hostport := struct {
+			Host string
+			Port string
+		}{
+			Host: "ip-10-31-81-235",
+			Port: fmt.Sprintf("%d", port),
+		}
+		c.Log("instance= ", hostport)
+		list = append(list, hostport)
+	}
+
+	data := make(map[string]interface{})
+	data["HostPortList"] = list
+	c.Log("Data= ", data)
+
+	// content
+	content := "Some content"
+	content_path := filepath.Join(os.TempDir(), "content.test")
+	err := ioutil.WriteFile(content_path, []byte(content), 0777)
+	c.Assert(err, Equals, nil)
+	content_url := "file://" + content_path
+
+	// Make up a template
+	config := fmt.Sprintf(`{ "file" : "{{file "%s"}}", "inline":"{{inline "%s"}}" }`,
+		content_url, content_url)
+
+	c.Log("Config:", config)
+
+	// write config to disk
+	config_path := filepath.Join(os.TempDir(), "config.test")
+	err = ioutil.WriteFile(config_path, []byte(config), 0777)
+	c.Assert(err, Equals, nil)
+
+	url := "file://" + config_path
+	applied, err := ExecuteTemplateUrl(nil, url, "", data)
+
+	c.Assert(err, Equals, nil)
+	c.Log("config= ", string(applied))
+
+	// parse the json and test
+	parsed := map[string]string{}
+
+	err = json.Unmarshal(applied, &parsed)
+	c.Assert(err, Equals, nil)
+	c.Log(parsed)
+
+	c.Assert(parsed["inline"], Equals, content)
+	// read the file
+	f, err := os.Open(parsed["file"])
+	c.Assert(err, Equals, nil)
+	buff, err := ioutil.ReadAll(f)
+	c.Assert(err, Equals, nil)
+	c.Assert(string(buff), Equals, content)
+}
+
+var nginx = `
+upstream backend {
+  {{range containers "/{{.Domain}}/{{.Service}}/live" "3000"}}
+    server {{.Host}}:{{.Port}};
+  {{end}}
+}
+
+server {
+
+       listen 443;
+       server_name *.infradash.com;
+
+       ssl on;
+       ssl_certificate {{file "env:///code.qor.io/ssl/qor.io.cert"}};
+       ssl_certificate_key {{file "env:///code.qor.io/ssl/qor.io.key"}};
+
+       root /var/www/infradash/public;
+       try_files $uri/index.html $uri @backend;
+
+       location @backend {
+
+             # Support for CORS
+	     # OPTIONS indicates a CORS pre-flight request
+	     if ($request_method = 'OPTIONS') {
+	       add_header 'Access-Control-Allow-Origin' "*";
+	       add_header 'Access-Control-Allow-Credentials' 'true';
+	       add_header 'Access-Control-Max-Age' 1728000;
+	       add_header 'Access-Control-Allow-Methods' 'GET, POST, PUT, OPTIONS, DELETE';
+	       add_header 'Access-Control-Allow-Headers' 'Authorization,Content-Type,Accept,Origin,User-Agent,DNT,Cache-Control,X-Mx-ReqToken,Keep-Alive,X-Requested-With,If-Modified-Since';
+	       add_header 'Content-Length' 0;
+	       add_header 'Content-Type' 'text/plain charset=UTF-8';
+	       return 204;
+	     }
+	     # non-OPTIONS indicates a normal CORS request
+	     if ($request_method = 'GET') {
+	       add_header 'Access-Control-Allow-Origin' "*";
+	       add_header 'Access-Control-Allow-Credentials' 'true';
+	     }
+	     if ($request_method = 'POST') {
+	       add_header 'Access-Control-Allow-Origin' "*";
+	       add_header 'Access-Control-Allow-Credentials' 'true';
+	     }
+	     if ($request_method = 'PUT') {
+	       add_header 'Access-Control-Allow-Origin' "*";
+	       add_header 'Access-Control-Allow-Credentials' 'true';
+	     }
+	     if ($request_method = 'DELETE') {
+	       add_header 'Access-Control-Allow-Origin' "*";
+	       add_header 'Access-Control-Allow-Credentials' 'true';
+	     }
+
+	     add_header 'X-infradash-Nginx-Template' 'v0.1';
+
+	     proxy_set_header Host $http_host;
+	     proxy_set_header X-Real-IP $remote_addr;
+	     proxy_set_header Client-IP $remote_addr;
+	     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+	     proxy_pass http://backend;
+	 }
+
+	 error_page 500 502 503 /500.html;
+  	 error_page 504 /504.html;
+	 client_max_body_size 1G;
+	 keepalive_timeout 10;
+}
+
+`
+
+func (suite *TestSuiteFetch) TestFetchAndExecuteTemplateNginxConf(c *C) {
+	// create some nodes
+	zk.CreateOrSet(suite.zc, registry.Path("/test.com/testapp/v1/containers/c1:3000"), "host1:8130")
+	zk.CreateOrSet(suite.zc, registry.Path("/test.com/testapp/v1/containers/c2:3000"), "host2:8131")
+	zk.CreateOrSet(suite.zc, registry.Path("/test.com/testapp/v1/containers/c3:3000"), "host3:8132")
+	zk.CreateOrSet(suite.zc, registry.Path("/test.com/testapp/v1/containers/c4:3100"), "host4:8133")
+
+	// Reference node
+	zk.CreateOrSet(suite.zc, registry.Path("/test.com/testapp"), "env:///test.com/testapp/v1/containers")
+
+	c.Log("Data inserted into zookeeper")
+
+	path := filepath.Join(os.TempDir(), "nginx.conf")
+	err := ioutil.WriteFile(path, []byte(nginx), 0777)
+	c.Assert(err, Equals, nil)
+	content_url := "file://" + path
+
+	applied, err := ExecuteTemplateUrl(suite.zc, content_url, "", map[string]string{
+		"Domain": "test.com", "Service": "testapp",
+	})
+
+	c.Assert(err, Equals, nil)
+	c.Log("config= ", string(applied))
+}
